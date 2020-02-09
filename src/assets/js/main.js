15fb7b54cc8c7c98438beeb9e45ac1d8208afbf8
Benedikt Gregor <runbgn@gmail.com>
2020-02-09 17:42:41 +0100

    basic setup

---
+++ src/assets/js/main.js
@@ -0,0 +1,16 @@
+var rates = [
+  {
+    id: 1,
+    amount: 13.5,
+    interval: "hourly"
+  }
+];
+
+var records = [
+  {
+    id: 1,
+    salary_id: 1,
+    begin: "2020-01-30 15:00",
+    end: "2020-01-30 22:00"
+  }
+];
