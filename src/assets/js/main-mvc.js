0d29d2507f5d6cf56659e73be374629529a3a552
Benedikt Gregor <runbgn@gmail.com>
2020-02-10 18:36:49 +0100

    controller (wip)

--- src/assets/js/main-mvc.js
+++ src/assets/js/main-mvc.js
@@ -1,13 +1,11 @@
 import sampleData from './../../data/sample-data.js'
 
-var app
-
 function Model() {
   this.records = sampleData.records
 
   this.addRecord = function(obj) {
     this.records.push({
-      // todo: get max id via reduce?
+      // todo: get max id with reduce()?
       id: this.records.length > 0 ? this.records[this.records.length - 1].id + 1 : 1,
       rate_id: 1,
       begin: obj.begin,
@@ -22,6 +20,8 @@ function Model() {
       return record.id !== id
     })
   }
+
+  this.bindRecordsListChanged = function(callback) {}
 }
 
 function View() {
@@ -45,16 +45,28 @@ function View() {
         var li = document.createElement('li')
         li.id = 'record-' + record.id
 
-        li.innerHTML = `Record #${record.id} - Begin: ${record.begin}`
+        li.innerHTML = `
+          Record #${record.id} - Begin: ${record.begin} <button class="record-delete">X</button>
+        `
         this.recordsList.append(li)
       })
     }
   }
 
   this.bindAddRecord = function(handler) {
-    this.form.addEventlistener('submit', function handleEvent(event) {
+    this.form.addEventListener('submit', function handleEvent(event) {
       event.preventDefault()
-      console.log('calling handler for event: ', handler)
+      console.log('calling handler for form submit event: ', handler)
+      // handler( {newRecordObj} )
+    })
+  }
+
+  this.bindDeleteRecord = function(handler) {
+    this.recordsList.addEventListener('click', function handleEvent(event) {
+      if (event.target.className == 'record-delete') {
+        console.log('calling handler for click on records list event if is button: ', handler)
+      }
+
       // handler( {newRecordObj} )
     })
   }
@@ -64,6 +76,11 @@ function Controller(model, view) {
   this.model = model
   this.view = view
 
+  this.init = function() {
+    // this.onRecordsListChanged(this.model.records)
+    this.view.displayRecords(this.model.records)
+  }
+
   this.handleAddRecord = function(record) {
     this.model.addRecord(record)
   }
@@ -72,12 +89,17 @@ function Controller(model, view) {
     this.model.deleteRecord(id)
   }
 
-  //Display initial Records
-  this.view.displayRecords(this.model.records)
+  this.onRecordsListChanged = function(records) {
+    this.view.displayRecords(records)
+  }
+
+  // Bind Evenet Listeners To Handlers
+  this.view.bindAddRecord(this.handleAddRecord)
+  this.view.bindDeleteRecord(this.handleDeleteRecord)
 }
 
-app = new Controller(new Model(), new View())
+var app = new Controller(new Model(), new View())
+
+app.init()
 
 window.app = app
-
-// console.log(app.model.records)
