f3520f024a136eef36645d11381817cab3386fca
Benedikt Gregor <runbgn@gmail.com>
2020-02-29 18:09:56 +0100

    split request in router

--- src/public/index.js
+++ src/public/index.js
@@ -1,9 +1,9 @@
 import Nav from './components/nav.js'
-// import MainView from './components/main-view.js'
 import sampleData from './data/sample-data.js'
 import Toolbar from './components/toolbar.js'
 import Home from './components/home.js'
 import RecordsList from './components/records-list.js'
+import RecordsListItem from './components/records-list-item.js'
 
 class App {
   set state(state) {
@@ -16,26 +16,22 @@ class App {
   }
 
   init(container) {
-    this.appDataPresent = localStorage.hasOwnProperty('appData')
     this.container = container
+
+    this.appDataPresent = localStorage.hasOwnProperty('appData')
     this.store = JSON.parse(localStorage.getItem('appData'))
+
     this.navContainer = this.container.querySelector('[data-main-nav]')
     this.mainViewContainer = this.container.querySelector('[data-main-view]')
-    this.state = { ui: 'default', appDataPresent: this.store ? true : false }
 
+    this.state = { ui: 'default', appDataPresent: this.store ? true : false }
+    this.prepareMainViewComponent()
     this.addEventListeners()
-    ;(function appDataIsPresent() {
-      // if (this.appDataPresent) {
-      //   if (this.store.app && this.store.app.version) {
-      //     console.log('local storage app version: ', this.store.app.version)
-      //   }
-      // }
-    })()
 
     this.forceUpgradeStorage()
   }
 
-  prepareView() {
+  prepareMainViewComponent() {
     if (this.viewComponent) {
       this.viewComponent.remove()
     }
@@ -43,17 +39,46 @@ class App {
     this.viewComponent.dataset.viewComponent = ''
   }
 
-  router() {
-    this.viewComponent = this.container.querySelector('[data-view-component]')
+  parseRequestURL() {
+    let url = window.location.pathname.toLowerCase() || '/'
+    let r = url.split('/')
+    let request = {
+      resource: null,
+      id: null,
+      verb: null
+    }
+    request.resource = r[1]
+    request.id = r[2]
+    request.verb = r[3]
 
-    switch (window.location.pathname) {
-      case '/':
-        this.prepareView()
+    return request
+  }
+
+  router() {
+    const request = this.parseRequestURL()
+
+    switch (request.resource) {
+      case '':
         new Home(this.viewComponent, { displayRecords: true })
         break
+      case 'records':
+        console.log(request.id)
+        if (request.verb) {
+          console.log('new Form, action: ', request.verb)
+        } else if (request.id) {
+          console.log('get single')
+          new RecordsListItem(
+            this.viewComponent,
+            this.store.records.filter(record => {
+              return record.id == request.id
+            })
+          )
+        } else {
+          new RecordsList(this.viewComponent)
+        }
 
-      case '/records':
-        this.prepareView()
+        break
+      case 'settings':
         new RecordsList(this.viewComponent)
         break
 
