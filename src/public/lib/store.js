a9ffeba1ec269bdc47b2cdf07b27fa3a4a24d9e9
Benedikt Gregor <runbgn@gmail.com>
2020-03-01 20:12:59 +0100

    Store Module (wip)

---
+++ src/public/lib/store.js
@@ -0,0 +1,24 @@
+export const Store = {
+  appData: JSON.parse(localStorage.getItem('appData')),
+
+  getRecord: function(id) {
+    var requestedRecord = this.appData.records.find(recordById)
+    return requestedRecord
+  },
+
+  get recordsMaxId() {
+    var maxId = 0
+    this.appData.records.forEach(record => {
+      if (maxId < record.id) {
+        maxId = record.id
+      }
+    })
+    return maxId
+  }
+}
+
+function recordById(record, id) {
+  return function() {
+    return record.id == id
+  }
+}
