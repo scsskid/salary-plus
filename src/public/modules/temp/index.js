707504314f344c5869a8871368c2b785e198b4c3
Benedikt Gregor <runbgn@gmail.com>
2020-03-02 02:33:38 +0100

    cleanup

--- src/public/modules/temp/index.js
+++
@@ -1,7 +0,0 @@
-import Model from './modules/model.js'
-import View from './modules/view.js'
-import Controller from './modules/controller.js'
-
-var app = new Controller(new Model(), new View())
-
-window.app = app
