f35a12860eda5060492c41de46d8ba75154190c5
Benedikt Gregor <runbgn@gmail.com>
2020-03-03 21:35:43 +0100

    cleanup

--- src/public/components/home.js
+++ src/public/components/home.js
@@ -15,6 +15,7 @@ export default class Home extends BaseComponent {
     new ButtonRecordNew(this.container.querySelector('[data-home-header]'))
     new Calendar(this.container.querySelector('[data-home-main]'))
 
+    // New Records List
     if (this.state.displayRecords) {
       var recordsListContainer = document.createElement('section')
       new RecordsList(recordsListContainer)
@@ -30,5 +31,3 @@ export default class Home extends BaseComponent {
     super(container, state)
   }
 }
-
-Home.refs = {}
