9deab07841cffa5aa69e675995ed1ba77cec4818
Benedikt Gregor <runbgn@gmail.com>
2020-03-06 03:59:32 +0100

    add routes file

---
+++ src/public/data/routes.js
@@ -0,0 +1,22 @@
+export default [
+  // ? idea: possibly extend with modules for other areas than main;
+  // ? e.g.:  modules: [ { container: this.header, file: 'special-header.js', state: { displaySth: true } }, { container: this.mainView, file: 'records-list.js' } ]
+  {
+    path: '/',
+    moduleFile: 'home.js',
+    data: { displayRecords: false }
+  },
+  // ? combine /records and /records/:recordsId
+  {
+    path: '/records',
+    moduleFile: 'records-list.js'
+  },
+  {
+    path: '/records/:recordId',
+    moduleFile: 'record.js'
+  },
+  {
+    path: '/settings',
+    moduleFile: 'settings.js'
+  }
+]
