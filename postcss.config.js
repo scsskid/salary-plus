842dde81abf0dd3debca0f0100ccd1bd6de1ac51
Benedikt Gregor <runbgn@gmail.com>
2020-03-10 18:01:29 +0100

    add postcss import, cssnano for production

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = ctx => ({
   map: ctx.options.map,
   parser: ctx.options.parser,
   plugins: {
-    // 'postcss-import': { root: ctx.file.dirname },
+    'postcss-import': { root: ctx.file.dirname },
     'postcss-preset-env': {
       stage: 4 // 0: polyfill all, 4: polyfill none, but [features: { 'nesting-rules': true}]
     },
