784f1df2c2012fccf8fb8d62b41c4055de0d3eb9
Benedikt Gregor <runbgn@gmail.com>
2020-03-11 03:04:32 +0100

    refactor

--- dist/app/components/Calendar.js
+++ dist/app/components/Calendar.js
@@ -1,6 +1,13 @@
 import BaseComponent from './BaseComponent.js'
 
 class Calendar extends BaseComponent {
+  init(container, state) {
+    this.container = container
+    this.state = state
+
+    this.createCalendar = createCalendar
+  }
+
   render() {
     this.container.innerHTML = `
       <style>
@@ -22,38 +29,48 @@ class Calendar extends BaseComponent {
     // const firstDay = new Date(year, month).getDay()
 
     // Construct Cal
-    this.createCalendar()
-  }
 
-  createCalendar() {
-    const table = document.createElement('table')
-    {
-      const dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      const row = document.createElement('tr')
-      for (let i = 0; i < 7; i++) {
-        const cell = document.createElement('th')
-        const cellText = document.createTextNode(dayNames[i])
-        cell.appendChild(cellText)
-        row.appendChild(cell)
-      }
-      table.appendChild(row)
-    }
+    const date = new Date()
 
-    let date = 1
-    for (let i = 0; i < 6; i++) {
-      const row = document.createElement('tr')
-      table.appendChild(row)
-      for (let j = 0; j < 7; j++) {
-        const cell = document.createElement('td')
-        const cellText = document.createTextNode('0')
-        cell.appendChild(cellText)
-        row.appendChild(cell)
-        console.log('waaaa')
-      }
-    }
-
-    this.calendar.appendChild(table)
+    this.createCalendar(date.getMonth(), date.getFullYear())
   }
 }
 
 export default Calendar
+
+// Calendar Helper Functions
+
+function createCalendar(month, year) {
+  const table = createTableOuter()
+
+  let date = 1
+  for (let i = 0; i < 6; i++) {
+    const row = document.createElement('tr')
+    table.appendChild(row)
+    for (let j = 0; j < 7; j++) {
+      const cell = document.createElement('td')
+      const cellText = document.createTextNode('0')
+      cell.appendChild(cellText)
+      row.appendChild(cell)
+    }
+  }
+
+  this.calendar.appendChild(table)
+}
+
+function createTableOuter() {
+  const table = document.createElement('table')
+  {
+    const dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    const row = document.createElement('tr')
+    for (let i = 0; i < 7; i++) {
+      const cell = document.createElement('th')
+      const cellText = document.createTextNode(dayNames[i])
+      cell.appendChild(cellText)
+      row.appendChild(cell)
+    }
+    table.appendChild(row)
+  }
+
+  return table
+}
