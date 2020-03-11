a0dfb8f20550b94b69a1501687f4833ecdf43dca
Benedikt Gregor <runbgn@gmail.com>
2020-03-11 04:15:20 +0100

    calendar first draft

--- dist/app/components/Calendar.js
+++ dist/app/components/Calendar.js
@@ -32,7 +32,7 @@ class Calendar extends BaseComponent {
 
     const date = new Date()
 
-    this.createCalendar(date.getMonth(), date.getFullYear())
+    this.createCalendar(date.getFullYear(), date.getMonth())
   }
 }
 
@@ -40,16 +40,40 @@ export default Calendar
 
 // Calendar Helper Functions
 
-function createCalendar(month, year) {
+function createCalendar(year, month) {
+  const inputDate = new Date(year, month)
+  // save firstWeekDay Int (Sun to Sat) to check for later
+  // and Adjust that Mon = 0
+  const firstDay = (inputDate.getDay() + 6) % 7 // Mo = 0; Sun = 6
+
+  console.log(year, month, firstDay)
+
   const table = createTableOuter()
 
   let date = 1
   for (let i = 0; i < 6; i++) {
     const row = document.createElement('tr')
     table.appendChild(row)
+
+    // Create Rows
     for (let j = 0; j < 7; j++) {
+      console.log(j)
+
       const cell = document.createElement('td')
-      const cellText = document.createTextNode('0')
+      let cellText
+      // fill empty cells until first date of manth is nth day
+      // check if is first row, and interationCount is less than firstDay
+      // todo: fill with last month dates
+      if (i == 0 && j < firstDay) {
+        console.log('empty')
+        cellText = document.createTextNode('😴')
+      } else if (date > daysInMonth(year, month)) {
+        cellText = document.createTextNode('😱')
+      } else {
+        cellText = document.createTextNode(date)
+        date++
+      }
+
       cell.appendChild(cellText)
       row.appendChild(cell)
     }
@@ -73,4 +97,8 @@ function createTableOuter() {
   }
 
   return table
+}
+
+function daysInMonth(iYear, iMonth) {
+  return 32 - new Date(iYear, iMonth, 32).getDate()
 }
